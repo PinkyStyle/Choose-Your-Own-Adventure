5e44861ac23a00e8b0e988d593e2cb11eeb0ca8d
Matias <pinkystylee98@gmail.com>
2019-09-17 01:54:23 -0300

    Creacion adapter
    se creo un adapter, wololo

---
+++ Main/src/ModuloDeGestorDeDatos/Adapter.java
@@ -0,0 +1,28 @@
+package ModuloDeGestorDeDatos;
+import ModuloDeCreacionLibroJuego.LibroJuego;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+public class Adapter {
+       
+    /**
+     * Metodo que transforma el librojuego a un archivo txt.
+     * @param librojuego El librojuego a transformar.
+     */
+    public void guardarLibroJuego(LibroJuego librojuego){
+        
+    }
+    
+    /**
+     * Metodo que transforma un archivo txt a un librjuego.
+     */
+    public void cargarLibroJuegos(){
+                
+    }
+    
+    
+}
