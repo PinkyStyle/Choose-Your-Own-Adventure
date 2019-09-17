e3f06ec656d287498b07d62e676654f1ee04af3e
Cristobal <cristobal.henriquez.g@gmail.com>
2019-09-17 01:08:50 -0300

    si la cago me avisan 2.0
    xD

--- Main/src/GestionDeJugarLibroJuego/Protagonista.java
+++ Main/src/GestionDeJugarLibroJuego/Protagonista.java
@@ -6,13 +6,58 @@ import java.util.ArrayList;
  * 
  */
 public class Protagonista {
-    private ArrayList<Artefacto> artefactos;
-    private ArrayList<Artefacto> artegactosQuemados;
-
+    private ArrayList<String> artefactos;
+    private ArrayList<String> artefactosQuemados;
+    private String nombre; 
     /**
      * Default constructor
      */
-    public Protagonista() {
+    public Protagonista(String nombre) {
+        this.nombre = nombre;
     }
 
+    public boolean buscarArtefacto(String artefacto) {
+        if(artefactos.contains(artefacto)){
+            return true
+        }
+        else {
+            return false;
+        }
+    }
+    public void setArtefactos(ArrayList<String> artefactos) {
+        this.artefactos = artefactos;
+    }
+    public void addArtefacto(String artefacto){
+        artefactos.add(artefacto);
+    }
+    public void eliminarArtefacto(String artefacto){
+        artefactos.remove(artefacto);  
+        addArtefactoQuemado(artefacto);
+    }
+    
+    public boolean buscarAtefactoQuemado(String artefacto){
+       if(artefactos.contains(artefacto)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public void setArtefactosQuemados(ArrayList<String> artegactosQuemados) {
+        this.artefactosQuemados = artegactosQuemados;
+    }
+    public void addArtefactoQuemado(String artefacto){
+        artefactosQuemados.add(artefacto);
+    }
+  
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
 }
